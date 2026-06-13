Effective degrees of freedom were added to the uncertainty calculation; the Compton edge uncertainty equation was corrected; correlation terms were included in the peak voltage uncertainty calculation; the parameter m was added to the input text file to calculate the effective number of degrees of freedom.

Speknife/detection_physics.py
@@ -67,7 +67,7 @@ def escape_correction(i, energy, corrected_data,tube_voltage,raw_data,a,b,escape
     corrected_data[i]=raw_data[i]-photoelectric_factor #replacing to the new correted values
     escape_corre[i] = photoelectric_factor
 
-def compton_correction(i,energy,corrected_data,a,b,corre_compton,data_uncertainty, energy_uncertainty, a_uncertainty, b_uncertainty):
+def compton_correction(i,energy,corrected_data,a,b,corre_compton,data_uncertainty, energy_uncertainty, a_uncertainty, b_uncertainty,r_ab):
     nist_energy, nist_abs = mfiles.reading_files('theoretical_functions_data/'+'compton_nist.txt')#opening nist data of absorption coeficient
     
     #Compton correction functions
@@ -75,8 +75,8 @@ def compton_correction(i,energy,corrected_data,a,b,corre_compton,data_uncertaint
     compton_edge_channel = round((compton_edge-b)/a) 
 
     #uncertainty of the compton edge energy
-    compton_edge_uncertainty = (4*energy*(2*energy+511)-2*(2+511)*(energy**2))*energy_uncertainty[i]/(2*energy+511)**2
-    compton_edge_channel_uncertainty = np.sqrt((compton_edge_uncertainty/a)**2+(b_uncertainty/a)**2+(((compton_edge-b)/a**2)*a_uncertainty)**2)
+    compton_edge_uncertainty = (2044*energy+4*energy**2)*energy_uncertainty[i]/(2*energy+511)**2
+    compton_edge_channel_uncertainty = np.sqrt((compton_edge_uncertainty/a)**2+(b_uncertainty/a)**2+(((compton_edge-b)/a**2)*a_uncertainty)**2-2*(compton_edge-b)*a_uncertainty*b_uncertainty*r_ab/a**3)
     
     if (compton_edge_channel>0): 
         #Happens for the initial channels and therefore we do nothing